37aa01c94b93efdcd57cab12496ec26c4c2c0c6d
C1H4N <bayramcihan2005@gmail.com>
2024-12-26 10:08:19 +0300

    Fix

--- next.config.js
+++ next.config.js
@@ -1,26 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'ktusdc.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-    formats: ['image/avif', 'image/webp'],
-  },
-  compress: true,
-  poweredByHeader: false,
-  reactStrictMode: true,
-  swcMinify: true,
-  compiler: {
-    removeConsole: process.env.NODE_ENV === 'production',
-  },
-  experimental: {
-    optimizeCss: true,
-  },
-}
+  /* config options here */
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
