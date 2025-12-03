03b46036a0246ff08e7283dede2d6adee1b2d29c
Murat C. Akyol <muratcan.akyol739@gmail.com>
2025-12-03 19:07:14 +0300

    fix: remove unused Image import

--- app/_components/Announcements.tsx
+++ app/_components/Announcements.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useEffect, useRef, useState } from "react";
-import Image from "next/image";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
